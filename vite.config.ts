1b37a7bf3450f76cbf2b455be4e373fbc0a63ef4
SOUTHDRAGON\tnaml <tnamlong2132000@gmail.com>
2025-11-06 11:55:26 +0700

    remove rebundant import

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import path from 'path';
-import checker from 'vite-plugin-checker';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
 const PORT = 3039;
 
 export default defineConfig({
-  plugins:  [],
+  plugins: [],
   resolve: {
     alias: [
       {
