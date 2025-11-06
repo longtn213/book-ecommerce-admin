8a66f726690ef9fb2bd0a40a88bb75e771b217c6
SOUTHDRAGON\tnaml <tnamlong2132000@gmail.com>
2025-11-06 10:00:27 +0700

    first commit

---
+++ src/layouts/nav-config-workspace.tsx
@@ -0,0 +1,14 @@
+import type { WorkspacesPopoverProps } from './components/workspaces-popover';
+
+// ----------------------------------------------------------------------
+
+export const _workspaces: WorkspacesPopoverProps['data'] = [
+  {
+    id: 'book-ecommerce',
+    name: 'Book-Ecommerce-Admin',
+    plan: 'Free',
+    logo: '/assets/icons/workspaces/logo-1.webp',
+  },
+
+
+];
